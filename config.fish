dd830367a975769f6dee731ccda070b0584fc537
reus <reusee@gmail.com>
2019-12-19 13:54:15 +0800

    update

--- config.fish
+++ config.fish
@@ -4,8 +4,10 @@ set -x fish_greeting ''
 export LANG=zh_CN.UTF-8
 
 set PATH $PATH /home/reus/gotip/bin 
-alias go 'env ALL_PROXY=http://127.0.0.1:9102 HTTP_PROXY=http://127.0.0.1:9102 SOCKS_PROXY=socks5://10.0.0.2 go'
-set PATH $PATH (go env GOROOT)/misc/wasm
+#set PATH $PATH (go env GOROOT)/misc/wasm
+alias wgo 'env GOOS=js GOARCH=wasm go'
+set -x GOPRIVATE 'github.com/reusee/*'
+set -x GOPATH $HOME
 
 alias a 'axel -n 10 -a'
 
@@ -17,7 +19,7 @@ alias gitp 'git push origin master'
 alias gitps 'git push site master'
 
 alias ll 'ls -ltr'
-alias l 'ls -lt'
+alias l 'ls -lv'
 
 alias bd "aria2c -c -k 2M -x 16 -s 16 --user-agent 'netdisk;5.3.1.3;PC;PC-Windows;5.1.2600;WindowsBaiduYunGuanJia' --header 'Referer:http://pan.baidu.com/disk/home' -o"
 
@@ -30,12 +32,34 @@ set -x LESS "-iMSx4 -FX"
 
 set -x PLAN9 /usr/lib/plan9
 set PATH $PATH $PLAN9/bin
+set -x venti 127.0.0.1
 
-set -x ALSA_CARD Generic
-#set -x ALSA_CARD M6
+#set -x ALSA_CARD Generic
+set -x ALSA_CARD M6
 
 alias startx 'startx >> ~/.xlogs ^^ ~/.xlogs'
 
-alias lsblk 'lsblk -o FSTYPE,LABEL,MOUNTPOINT,SIZE,SCHED,NAME'
+alias lsblk 'lsblk -o FSTYPE,LABEL,MOUNTPOINT,SIZE,SCHED,NAME,PARTUUID'
 
 alias dn dotnet
+
+set PATH $PATH /media/build/flutter/bin
+
+set -x ENABLE_FLUTTER_DESKTOP true
+
+set -x CHROME_EXECUTABLE chromium
+
+function volumedetect
+  ffmpeg -i $argv -af volumedetect -vcodec copy -f null /dev/null
+end
+
+alias PROXY 'env ALL_PROXY=socks5://localhost:9103 HTTP_PROXY=http://localhost:9102 SOCKS_PROXY=socks5://localhost:9103 HTTPS_PROXY=socks5://localhost:9103'
+
+set -x FREETYPE_PROPERTIES "truetype:interpreter-version=35"
+
+set -x VK_ICD_FILENAMES "/usr/share/vulkan/icd.d/amd_icd64.json" 
+
+alias r 'rsync -avvz --partial-dir=.rsync-partial'
+
+set -x CGO_CFLAGS_ALLOW '.*'
+
